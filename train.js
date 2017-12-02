a7a4bf7f364683398b8ef92771127a1c9e279842
in1gma <wmzhkm@gmail.com>
2017-12-02 17:53:46 +0500

    train fix

--- train.js
+++ train.js
@@ -1,9 +1,10 @@
-var train = (perceptron, epochs, learningRate, actFunc, getFunc) => {
+var train = (perceptron, epochs, learningRate, actFunc, items) => {
 	for (let i = 0; i < epochs; i++) {
-		for (let [data, actual] of getFunc) {
+		for (let [data, actual] of Object.entries(items)) {
 			let expected = perceptron.process(data, actFunc)
 			let delta = expected - actual
 			perceptron.adjust(data, delta, learningRate)
+			console.log(delta)
 		}
 	}
 }
