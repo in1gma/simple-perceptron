5147421b86f275255d8cfb924e635b0c85c304ae
in1gma <wmzhkm@gmail.com>
2017-12-02 16:09:42 +0500

    processing and adjusting perceptron

--- perceptron.js
+++ perceptron.js
@@ -11,4 +11,17 @@ class Perceptron {
 	set bias (bias) {
 		this._bias = bias
 	}
+	process (inputs, func) {
+		let sum = this._bias
+		for (let i = 0; i < inputs.length; i++) {
+			sum += inputs[i] * this._weights[i];
+		}
+		return func(sum);
+	}
+	adjust (inputs, delta, learningRate) { // learningRate ~ (0, 1]
+		for (let i = 0; i < inputs.length; i++) {
+			this._weights[i] += learningRate * delta * inputs[i] 
+		}
+		this._bias += learningRate * delta
+	}
 }
